d7cb2d0171cb2beb345225d913400697e202c374
JefersonM123 <jefersonmartinsborges@hotmail.com>
2017-08-20 16:33:35 -0300

    .

--- DeutschScript/DeutschScript/Program.cs
+++ DeutschScript/DeutschScript/Program.cs
@@ -1,21 +1,98 @@
 ﻿using System;
 using System.Windows.Forms;
 using ConsoleHotKey;
+using System.IO;
+using LexicalAnalyzer;
 
 namespace DeutschScript
 {
     class Program
     {
+
+        static string script = "";
+        ParserLexical parserLexical = new ParserLexical();
+
         static void Main(string[] args)
         {
-            HotKeyManager.RegisterHotKey(Keys.A, KeyModifiers.Alt);
-            HotKeyManager.HotKeyPressed += new EventHandler<HotKeyEventArgs>(HotKeyManager_HotKeyPressed);
-            Console.ReadLine();
+            while (true)
+            {
+                Header();
+                Console.Write(script);
+                script += Console.ReadLine();
+                script += "\n";
+                saveScript();
+            }
+
+            Console.WriteLine("Saindo");
+            Console.ReadKey();
         }
 
         static void HotKeyManager_HotKeyPressed(object sender, HotKeyEventArgs e)
         {
-            Console.WriteLine("Hit me!");
+            if (e.Modifiers.ToString() == "Control" && e.Key.ToString() == "S")
+            {
+                string name = FileName();
+                bool save = false;
+                do
+                {
+                    save = ValidateFile(name);
+                    if (!save)
+                        name = FileName();
+
+                } while (!save);
+
+                System.IO.File.WriteAllText(@"~Files/" + name + ".ds", script);
+                Console.WriteLine("Salvar");
+            }
+            else if (e.Modifiers.ToString() == "Control" && e.Key.ToString() == "Q")
+            {
+                Console.Write(script);
+                Console.ReadKey();
+                Environment.Exit(1);
+            }
+        }
+
+        static void Header()
+        {
+            Console.Clear();
+            //Console.WriteLine("CTRL + S - Salvar arquivo");
+            //Console.WriteLine("CTRL + N - Novo arquivo");
+            //Console.WriteLine("CTRL + Q - Sair");
+            Console.WriteLine("Deutsch Script");
+            Console.WriteLine();
+        }
+
+        static string FileName()
+        {
+            Header();
+            Console.Write("Nome do Arquivo: ");
+            string nome = Console.ReadLine();
+            return nome;
+        }
+
+        static bool ValidateFile(string fileName)
+        {
+            bool save = false;
+            if (File.Exists(fileName + ".ds"))
+            {
+                string sub = "";
+                while (!sub.Equals("S") && !sub.Equals("s") && !sub.Equals("N") && !sub.Equals("n"))
+                {
+                    Header();
+                    Console.Write("Arquivo " + fileName + ".ds já existe, deseja substituir?[S/N] ");
+                    sub = Console.ReadLine();
+                    save = (sub.Equals("S") ? true : false);
+                }
+            }
+            return save;
+        }
+
+        static void saveScript()
+        {
+            StreamWriter textValue = new StreamWriter(@"F:\Projetos\DeutschScript\DeutschScript\DeutschScript\Files\DeutschScript.ds", true);
+            textValue.Flush();
+            textValue.Write(script);
+            textValue.Close();
         }
     }
 }
---
+++ DeutschScript/LexicalAnalyzer/ParserLexical.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LexicalAnalyzer
+{
+    public class ParserLexical
+    {
+        public static List<String> reservedWord;
+
+        private string image, kind;
+        private int index, line;
+
+        public string Image { get => image; set => image = value; }
+        public string Kind { get => kind; set => kind = value; }
+        public int Index { get => index; set => index = value; }
+        public int Line { get => line; set => line = value; }
+
+        public ParserLexical()
+        {
+
+        }
+    }
+}
