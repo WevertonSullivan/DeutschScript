9f7774f021440e877d997952cb9e67b354432037
JefersonM123 <jefersonmartinsborges@gmail.com>
2017-12-10 13:19:44 -0200

    Adicionado dicionário DS para C

---
+++ DeutschScript/Common/DS_C.cs
@@ -0,0 +1,16 @@
+﻿namespace Common
+{
+    class DS_C
+    {
+        string _C, _DS;
+
+        public DS_C(string DS, string C)
+        {
+            this._C = C;
+            this._DS = DS;
+        }
+
+        public string C { get => _C; set => _C = value; }
+        public string DS { get => _DS; set => _DS = value; }
+    }
+}
---
+++ DeutschScript/Common/DStoC.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Common
+{
+    public class DStoC
+    {
+        private static List<DS_C> dsc = new List<DS_C>()
+        {
+            new DS_C("$", "//"), new DS_C("alle", "int"), new DS_C("text", "string"), new DS_C("real", "real"),
+            new DS_C("logisch", "bool"), new DS_C("leer", "void"), new DS_C("haupt", "main"), new DS_C("show","cout"),
+            new DS_C("lessen", "cin"), new DS_C("wenn", "if"), new DS_C("sonst", "else"), new DS_C("zum", "for"), new DS_C("out", "return"),
+            new DS_C("[", "("), new DS_C("]", ")"), new DS_C("<<", "{"), new DS_C(">>", "}"), new DS_C(".", ";"), new DS_C("#", "\""),
+            new DS_C("+", "+"), new DS_C("-", "-"), new DS_C("*", "*"), new DS_C("/", "/"), new DS_C("<-", "="),new DS_C(">", ">"), new DS_C("<", "<"),
+            new DS_C(">=",">="), new DS_C("<=", "<="), new DS_C("==", "=="), new DS_C("!=", "!="), new DS_C("&&", "&&"), new DS_C("||", "||")
+        };
+
+        public static string getCSymbol(string DSSymbol)
+        {
+            return dsc.Find(x => x.DS == DSSymbol).C;
+        }
+
+    }
+}
